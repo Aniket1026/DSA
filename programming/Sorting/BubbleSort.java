2c4ffacae6f1ffc3b824f97b1966db03559229cc
Aniket <annudubey1026@gmail.com>
2023-04-16 23:25:52 +0530

    bubble sort added

--- programming/Sorting/BubbleSort.java
+++ programming/Sorting/BubbleSort.java
@@ -8,24 +8,30 @@ import java.util.Arrays;
 -> if none of the element was swapped which indicates that the array is sorted already
 -> and we do not need to check any further. So it breaks the loop to stop the unnecessary iterations.
 -> This way you can optimize the bubble sort algorithm
+
+Time Complexity
+Best case -> O(n)
+Worst case -> O(n^2)
+
+Space complexity -O(1) as no extra space is used
  */
 public class BubbleSort {
 	public static void main(String[] args) {
-		int[] arr ={3,4,2,5,1,16,7,55,45,40};
+		int[] arr ={3,1,5,2,4,9,-6,33,20};
 		bubbleSort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
 	public static void bubbleSort(int[] arr){
 		boolean swapped;
-		for(int i=0;i< arr.length;i++){
-			swapped = true;
+		for(int i=0;i< arr.length-1;i++){
+			swapped = false;
 			for(int j=0;j< arr.length-1-i;j++){
 				if(arr[j]>arr[j+1]){
 					swap(arr,j,j+1);
 					swapped = true;
 				}
 			}
-			if(swapped==false){
+			if(!swapped){
 				break;
 			}
 		}
---
+++ programming/Sorting/SelectionSort.java
@@ -0,0 +1,2 @@
+package programming.Sorting;public class SelectionSort {
+}
