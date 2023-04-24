fc86c2759f8ab55887c3e23de9f13bef18d81e38
Aniket <annudubey1026@gmail.com>
2023-04-24 10:48:37 +0530

    About modified cyclic sort

--- programming/Sorting/ModifiedCyclicSort.java
+++ programming/Sorting/ModifiedCyclicSort.java
@@ -1,6 +1,22 @@
 package programming.Sorting;
-
 import java.util.Arrays;
+
+/*
+The modified Cyclic sort is very similar to cyclic sort the only difference is
+in normal cyclic sort numbers ranges from 1 to n or 0 to n;
+But this is the general version where the elements range from same value m to m+n;
+where n is the size of array.
+
+Approach:
+The array index ranges from 0 to n-1. For the range of numbers from m to m+n. The value at any index of the
+array is going to be value = index+m.
+-> Check if the value is at its correct index
+-> Swap the value with its correct index
+-> If the value is at the correct index only then move ahead to next element
+
+Time complexity : O(n) linear
+Auxiliary space : O(1) constant
+ */
 
 public class ModifiedCyclicSort {
 	public static void main(String[] args) {
