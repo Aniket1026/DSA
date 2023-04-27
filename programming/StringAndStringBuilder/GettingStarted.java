75d4eb588bdfb649c7ce0414f5b78aa5ee763cf1
Aniket <annudubey1026@gmail.com>
2023-04-28 00:55:00 +0530

    Strings introduced

--- programming/StringAndStringBuilder/GettingStarted.java
+++ programming/StringAndStringBuilder/GettingStarted.java
@@ -8,6 +8,8 @@ public class GettingStarted {
 	public static void main(String[] args) {
 		String a = "Aniket";
 		System.out.println(a);
+		System.out.println(89);
+		System.out.println(new int[] {1,2,3,4});
 		a ="Anubhav"; // this is valid because now a is pointing to new object
 	}
 }
