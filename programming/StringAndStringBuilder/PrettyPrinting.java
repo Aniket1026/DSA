c9729ddb523c94e2887f4f5f9ec710eee076dc8f
Aniket <annudubey1026@gmail.com>
2023-04-28 00:59:15 +0530

    Examples for pretty formatting

---
+++ programming/StringAndStringBuilder/PrettyPrinting.java
@@ -0,0 +1,9 @@
+package programming.StringAndStringBuilder;
+
+public class PrettyPrinting {
+	public static void main(String[] args) {
+		float a = 3.25361f;
+		System.out.printf("Formatted value is %.2f",a);
+		System.out.printf("Hello I'm %s and my brother is %s", "Aniket","Anubhav");
+	}
+}
